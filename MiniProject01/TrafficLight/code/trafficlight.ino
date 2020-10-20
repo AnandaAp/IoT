b0d898ede2c30f1ceeb34190c84eea0f0e45a21d
AnandaAp <42529423+AnandaAp@users.noreply.github.com>
2020-10-20 23:05:20 +0700

    Add files via upload

--- MiniProject01/TrafficLight/code/trafficlight.ino
+++ MiniProject01/TrafficLight/code/trafficlight.ino
@@ -7,59 +7,18 @@ const int ledPinGreen = 23;
    Jadi ketika pertama kali dijalankan,maka 
    seluruh led akan menyala terlebih dahulu
 */
-int ledStateRed = LOW;             
-int ledStateYellow = LOW;
-int ledStateGreen = LOW;
 
 /* unsigned long berfungsi untuk menyimpan data yang bersifat "menyimpan waktu"
    karena int tidak mampu untuk menyimpan waktu
 */
 unsigned long previousMillis = 0;        // akan menyimpah waktu terakhir led dinyalakan
 
-// interval yang aku gunakan adalah 3 detik
-const long interval = 3000;           // interval dalam satuan milisecond
+// interval yang aku gunakan:
+const long invRed = 10000;           // interval dalam satuan milisecond
+const long invYellow = 3000;
+const long invGreen = 6000;
 
-//method yg berfungsi untuk merubah status led
-void runTrafficLight(){
-  digitalWrite(ledPinRed, ledStateRed);
-  digitalWrite(ledPinYellow, ledStateYellow);
-  digitalWrite(ledPinGreen, ledStateGreen);
-}
-
-void trafficLight(long currentMillis){
-  if (currentMillis - previousMillis >= interval) {
-    // simpan detik terakhir led menyala
-    previousMillis = currentMillis;
-
-    /*
-      Posisi awal led adalah semua menyala selama 3 detik untuk permulaan
-      kemudian led merah akan menyala sendiri selama 3 detik
-      kemudian pada detik ke-3, led merah mati dan led kuning menyala selama 3 detik
-      lalu setelah detik ke-6, led kuning mati dan led hijau menyala selama 3 detik
-      dan kemudian pada detik ke-9, led merah kembali menyala dan led hijau mati
-    */
-    if (ledStateRed == LOW && ledStateYellow == LOW && ledStateGreen == LOW) {
-      ledStateRed = LOW;
-      ledStateYellow = HIGH;
-      ledStateGreen = HIGH;
-    }else if (ledStateRed == LOW && ledStateYellow == HIGH && ledStateGreen == HIGH) {
-      ledStateRed = HIGH;
-      ledStateYellow = LOW;
-      ledStateGreen = HIGH;
-    } else if(ledStateRed == HIGH && ledStateYellow == LOW && ledStateGreen == HIGH){
-      ledStateRed = HIGH;
-      ledStateYellow = HIGH;
-      ledStateGreen = LOW;
-    }
-    else if(ledStateRed == HIGH && ledStateYellow == HIGH && ledStateGreen == LOW){
-      ledStateRed = LOW;
-      ledStateYellow = HIGH;
-      ledStateGreen = HIGH;
-    }
-    runTrafficLight();
-  }
-}
-
+int arahLed = 0;
 
 void setup() {
   pinMode(ledPinRed, OUTPUT);
@@ -69,9 +28,39 @@ void setup() {
 }
 
 void loop() {
+  trafficLight();
+}
+
+void trafficLight(){
   /* 
     Variable digunakan untuk mengambil detik sekarang sebagai patokan
   */
   unsigned long currentMillis = millis();
-  trafficLight(currentMillis);
+  if(arahLed == 0){
+    digitalWrite(ledPinRed,LOW);
+    digitalWrite(ledPinYellow,HIGH);
+    digitalWrite(ledPinGreen,HIGH);
+    arahLed++;
+    previousMillis += invRed;
+  }
+  else{
+    if(arahLed == 1 && previousMillis - currentMillis == 0){
+      digitalWrite(ledPinRed,HIGH);
+      digitalWrite(ledPinYellow,LOW);
+      arahLed++;
+      previousMillis += invYellow;
+    }
+    else if(arahLed == 2 && previousMillis - currentMillis == 0){
+      arahLed++;
+      digitalWrite(ledPinYellow,HIGH);
+      digitalWrite(ledPinGreen,LOW);
+      previousMillis += invGreen;
+    }
+    else if(arahLed == 3 && previousMillis - currentMillis == 0){
+      digitalWrite(ledPinRed,LOW);
+      digitalWrite(ledPinGreen,HIGH);
+      arahLed = 1;
+      previousMillis += invRed;
+    }
+  }
 }
