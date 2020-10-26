bd550772e5026731462c68e59b5fd815955dec94
AnandaAp <42529423+AnandaAp@users.noreply.github.com>
2020-10-26 22:26:01 +0700

    mengupdate bug dan penyempurnaan sistem

--- MiniProject02/HumidityCensorWithESP32/code/Humidity_Sencor_With_ESP32_and_DHT_11.ino
+++ MiniProject02/HumidityCensorWithESP32/code/Humidity_Sencor_With_ESP32_and_DHT_11.ino
@@ -1,6 +1,21 @@
 #include "DHT.h"
 #define DHTPIN 4
 #define DHTTYPE DHT11
+
+/*
+ Program ini merupakan program otomasi untuk mengukur suhu dengan sensor, 
+ dimana untuk menyalakan Perangkat hanya perlu untuk menekan tombol
+ dan untuk mematikan sistem hanya perlu dengan menekan dan menahan tombol 
+ selama beberapa detik.
+ Catatan, ketika proses mengukur suhu berlangsung, sistem tidak dapat dimatikan
+ sampai sistem menunjukan hasil pengukuran suhu
+
+ 
+ Dibuat dan disusun oleh Ananda Apriliansyah
+ Credit for Ananda Apriliansyah Cahya Utama, a Informatic Student of Duta Wacana Christian University Yogyakarta, Indonesia
+*/
+
+
 //membuat object DHT bernama dht dengan pin dan tipenya
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -11,7 +26,7 @@ const int ledPinGreen = 23;
 const int btn = 0;
 
 int btnState = 0; //logic button low
-int run;
+int run; //variable untuk menentukan otomasi berjalan atau tidak
 
 //membaca temperatur menjadi celcius
 float temperaturCelcius;
@@ -31,8 +46,11 @@ void setup() {
 }
 
 void loop() {
+  //memberikan value dari button kepada btnState
   btnState = digitalRead(btn);
+  //mengecek apakah program sedang berjalan dengan nilai run lebih dari 0
   if(run > 0){
+    //mengecek apakah button ditekan, jika ditekan maka akan mematikan sistem
     if(btnState == LOW){
       run = 0;
       Serial.println("Mematikan Sistem");        
@@ -40,38 +58,52 @@ void loop() {
       digitalWrite(ledPinRed,HIGH);
       digitalWrite(ledPinYellow,HIGH);
       digitalWrite(ledPinGreen,HIGH);
+      Serial.println("Berhasil Mematikan Sistem");
       return;
     }
+    //jika tidak maka kembali menjalankan method tempDetect
     else{
-      HumidDetect();
+      tempDetect();
     }
   }
+  /*jika aplikasi belum berjalan dan tombol ditekan, 
+  maka sistem akan mejalankan pengukuran suhu*/
   else if(run == 0 && btnState == LOW){
     Serial.println("Button Ditekan, Memulai Sistem");
+    Serial.println("sistem membutuhkan waktu 4 detik untuk memulai menjalankan otomasi");
+    //sistem membutuhkan waktu 4 detik untuk memulai menjalankan otomasi
     delay(4000);
     Serial.println("MEMULAI MENGUKUR SUHU!!!!!!");
+    //merubah nilai run menjadi 255 jika nilai run masih 0
     if(run == 0){
       run = 255;
     }
   }
 }
 
-void HumidDetect(){
+void tempDetect(){
+    //memberikan delay 2 detik untuk memulai pengukuran suhu
     delay(2000);
     //membaca temperatur menjadi celcius
     temperaturCelcius = dht.readTemperature();
-    //mengecek apakah berhasil mengukur suhu
+    //mengecek apakah gagal mengukur suhu
     if(isnan(temperaturCelcius)){
       Serial.println("gagal mengukur suhu");
       Serial.println("kembali melakukan pengukuran mengukur");
-      HumidDetect();
+      tempDetect();
     }
-    Serial.print(F("Temperature: "));
-    Serial.print(temperaturCelcius);
-    Serial.print("°C \n");
-    checkTemp();
-    delay(2000);
+    //jika sukses mengukur suhu
+    else{
+      Serial.print(F("Temperature: "));
+      Serial.print(temperaturCelcius);
+      Serial.print("°C \n");
+      checkTemp();
+      //memberi delay 2 detik untuk mengecek apakah button kembali ditekan atau tidak
+      delay(2000);
+    }
 }
+
+//method untuk mengecek suhu dan menyalakan led
 void checkTemp(){
   delay(500);
   if(temperaturCelcius < 35){
